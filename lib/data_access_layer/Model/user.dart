209465adb5c5d9b45a26e6db3fe632a823ed8f35
Omar Hesham <oommaarr.220022@gmail.com>
2022-01-28 01:25:57 +0200

    Comment User Model

--- lib/data_access_layer/Model/user.dart
+++ lib/data_access_layer/Model/user.dart
@@ -6,6 +6,7 @@ class User {
   User({required this.user, required this.password});
 
 
+  /// Get user data from database record [dbrecord]
   static User fromDB(String dbrecord) {
     return User(user: dbrecord.split(':')[0], password: dbrecord.split(':')[1]);
   }

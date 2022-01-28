568b66cdbae165cd86ed1f8bb2bbf1e9f7b57713
Omar Hesham <oommaarr.220022@gmail.com>
2022-01-28 06:18:45 +0200

    Initial Intro Screen

---
+++ lib/GUI/intro_screen.dart
@@ -0,0 +1,222 @@
+import 'package:auth_via_tf_facial_recognition/GUI/Authentication/signup.dart';
+import 'package:auth_via_tf_facial_recognition/data_access_layer/Service/camera_service.dart';
+import 'package:auth_via_tf_facial_recognition/data_access_layer/Service/database.dart';
+import 'package:auth_via_tf_facial_recognition/data_access_layer/Service/detection_service.dart';
+import 'package:auth_via_tf_facial_recognition/data_access_layer/Service/ml_kit_service.dart';
+import 'package:auth_via_tf_facial_recognition/GUI/Authentication/login.dart';
+import 'package:flutter/material.dart';
+
+
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+
+  final DetectionService _detectionService = DetectionService();
+  final MLKitService _mlKitService = MLKitService();
+  final DatabaseService _dataBaseService = DatabaseService();
+  final CameraService _cameraService = CameraService();
+
+  bool loading = false;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _startUp();
+  }
+
+
+  /// Initializes the services
+  _startUp() async {
+    _setLoading(true);
+
+    await _cameraService.getFrontCamera();
+    await _detectionService.loadModel();
+    await _dataBaseService.load();
+    _mlKitService.init();
+
+    _setLoading(false);
+  }
+
+
+  /// Controls the visibility of circular progress indicator
+  _setLoading(bool value) {
+    setState(() {
+      loading = value;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFFC7FFBE),
+      appBar: AppBar(
+        leading: Container(),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 20, top: 20),
+            child: PopupMenuButton<String>(
+              child: const Icon(
+                Icons.more_vert,
+                color: Colors.black,
+              ),
+              onSelected: (value) {
+                switch (value) {
+                  case 'Clear DB':
+                    _dataBaseService.clear();
+                    break;
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                return {'Clear DB'}.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
+            ),
+          ),
+        ],
+      ),
+      body: !loading
+          ? SafeArea(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    const Image(image: AssetImage('assets/logo.png')),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Column(
+                        children: const [
+                          Text(
+                            "FACE RECOGNITION AUTHENTICATION",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Demo application that uses Flutter and tensorflow to implement authentication with facial recognition",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) => const Login(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                  color: Colors.blue.withOpacity(0.1),
+                                  blurRadius: 1,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'LOGIN',
+                                  style: TextStyle(color: Color(0xFF0F0BDB)),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Icon(Icons.login, color: Color(0xFF0F0BDB))
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) => const Signup(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: const Color(0xFF0F0BDB),
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                  color: Colors.blue.withOpacity(0.1),
+                                  blurRadius: 1,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'SIGN UP',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Icon(Icons.person_add, color: Colors.white)
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: const Divider(
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
